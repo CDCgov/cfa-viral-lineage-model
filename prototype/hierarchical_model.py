ec32a308b6b8a44ec09132f4161dcce6f2210aeb
Thanasi Bakis <abakis@uci.edu>
2024-06-21 11:32:03 -0700

    Fix for mcmc correlation

--- prototype/hierarchical_model.py
+++ prototype/hierarchical_model.py
@@ -11,26 +11,26 @@ from numpyro.infer import MCMC, NUTS
 
 def numpyro_model(
     counts: np.ndarray,
-    regions: np.ndarray,
+    divisions: np.ndarray,
     days: np.ndarray,
 ):
     """
-    Observations are counts of lineages for each region-day.
+    Observations are counts of lineages for each division-day.
     See https://doi.org/10.1101/2023.01.02.23284123
 
     counts:         A matrix of counts with shape (num_observations, num_lineages).
-    regions:        A vector of indices representing the region of each observation.
+    divisions:      A vector of indices representing the division of each observation.
     days:           A vector of the time covariate for each observation.
     """
 
     num_lineages = counts.shape[1]
-    num_regions = len(np.unique(regions))
+    num_divisions = len(np.unique(divisions))
 
-    # beta_0[g, l] is the intercept for lineage l in region g
+    # beta_0[g, l] is the intercept for lineage l in division g
     beta_0 = numpyro.sample(
         "beta_0",
         dist.Normal(0, 1),
-        sample_shape=(num_regions, num_lineages),
+        sample_shape=(num_divisions, num_lineages),
     )
 
     # mu_beta1[l] is the mean of the slope for lineage l
@@ -40,15 +40,17 @@ def numpyro_model(
         sample_shape=(num_lineages,),
     )
 
-    # beta_1[g, l] is the slope for lineage l in region g
-    beta_1 = numpyro.sample(
-        "beta_1",
-        dist.Normal(mu_beta1, 1),
-        sample_shape=(num_regions,),
+    # beta_1[g, l] is the slope for lineage l in division g
+    beta_1_uncentered = numpyro.sample(
+        "beta_1_uncentered",
+        dist.Normal(0, 1),
+        sample_shape=(num_divisions, num_lineages),
     )
 
-    # z[g, l] is the unnormalized probability of lineage l for observation g
-    z = beta_0[regions, :] + beta_1[regions, :] * days[:, None]
+    beta_1 = numpyro.deterministic("beta_1", beta_1_uncentered + mu_beta1)
+
+    # z[i, l] is the unnormalized probability of lineage l for observation i
+    z = beta_0[divisions, :] + beta_1[divisions, :] * days[:, None]
 
     numpyro.sample(
         "Y",
@@ -74,11 +76,12 @@ if __name__ == "__main__":
         .fill_null(0)
     )
 
-    # Extract count matrix, regions, and time covariate
+    # Extract count matrix, division indices, and time covariate
 
-    counts = data.drop(["date", "division", "day"])
+    counts = data.drop(["date", "division"])
+    counts = counts.select(sorted(counts.columns))
 
-    regions_key, regions_encoded = np.unique(data["division"], return_inverse=True)
+    divisions_key, divisions_encoded = np.unique(data["division"], return_inverse=True)
 
     days = data.with_columns(
         (pl.col("date").cast(pl.Date) - pl.date(2024, 5, 1))
@@ -91,28 +94,28 @@ if __name__ == "__main__":
     mcmc = MCMC(
         NUTS(numpyro_model),
         num_samples=1000,
-        num_warmup=200,
+        num_warmup=500,
     )
 
     mcmc.run(
         PRNGKey(0),
         counts.to_numpy(),
-        regions_encoded,
+        divisions_encoded,
         days,
     )
 
     # Export parameter samples
 
     beta1 = np.asarray(mcmc.get_samples()["beta_1"])
-    n_iterations, n_regions, n_lineages = beta1.shape
+    n_iterations, n_divisions, n_lineages = beta1.shape
 
     regional_growth_rates = pl.DataFrame(
         {
             "iteration": np.repeat(
-                np.arange(1, n_iterations + 1), n_regions * n_lineages
+                np.arange(1, n_iterations + 1), n_divisions * n_lineages
             ),
-            "region": np.tile(np.repeat(regions_key, n_lineages), n_iterations),
-            "lineage": np.tile(np.tile(counts.columns, n_regions), n_iterations),
+            "division": np.tile(np.repeat(divisions_key, n_lineages), n_iterations),
+            "lineage": np.tile(np.tile(counts.columns, n_divisions), n_iterations),
             "growth_rate": beta1.flatten(),
         }
     )
@@ -123,7 +126,7 @@ if __name__ == "__main__":
     global_growth_rates = pl.DataFrame(
         {
             "iteration": np.repeat(np.arange(1, n_iterations + 1), n_lineages),
-            "region": "[Global]",
+            "division": "[Global]",
             "lineage": np.tile(counts.columns, n_iterations),
             "growth_rate": mu_beta1.flatten(),
         }
@@ -131,7 +134,7 @@ if __name__ == "__main__":
 
     df = (
         pl.concat([regional_growth_rates, global_growth_rates])
-        .sort(["iteration", "region", "lineage"])
+        .sort(["iteration", "division", "lineage"])
         .with_columns(fitness_advantage=((pl.col("growth_rate") * 7).exp() - 1))
     )
 
