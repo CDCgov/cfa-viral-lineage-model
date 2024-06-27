1d9ca7a9fc5f94d8a8f685f39438b5236187de1e
Thanasi Bakis <abakis@uci.edu>
2024-06-27 09:28:00 -0700

    Add posterior means to viz

--- prototype/visualize.R
+++ prototype/visualize.R
@@ -21,22 +21,48 @@ max_a_posteriori <- samples |>
 	rename(map_phi = phi) |>
 	select(division, lineage, t, map_phi)
 
+means <- samples |>
+	group_by(division, t, lineage) |>
+	summarize(
+		mean_phi = mean(phi)
+	)
+
 quantiles <- samples |>
 	group_by(division, t, lineage) |>
 	summarize(
-		lower = quantile(phi, 0.1),
-		upper = quantile(phi, 0.9)
+		q_lower = quantile(phi, 0.1),
+		q_upper = quantile(phi, 0.9)
 	)
 
-full_join(max_a_posteriori, quantiles, by = c("division", "t", "lineage")) |>
+summaries <- means |>
+	full_join(quantiles, by = c("division", "t", "lineage")) |>
+	full_join(max_a_posteriori, by = c("division", "t", "lineage"))
+
+summaries |>
 	filter(str_starts(division, "A")) |>
 	ggplot() +
 	geom_ribbon(
-		aes(t, ymin = lower, ymax = upper, group = lineage, fill = lineage),
+		aes(t, ymin = q_lower, ymax = q_upper, group = lineage, fill = lineage),
 		alpha = 0.15
 	) +
 	geom_line(
 		aes(t, map_phi, group = lineage, color = lineage),
+		linewidth = 1.5
+	) +
+	facet_wrap(vars(division)) +
+	theme_bw(base_size = 20) +
+	ylab(expression(phi))
+
+
+summaries |>
+	filter(str_starts(division, "A")) |>
+	ggplot() +
+	geom_ribbon(
+		aes(t, ymin = q_lower, ymax = q_upper, group = lineage, fill = lineage),
+		alpha = 0.15
+	) +
+	geom_line(
+		aes(t, mean_phi, group = lineage, color = lineage),
 		linewidth = 1.5
 	) +
 	facet_wrap(vars(division)) +
