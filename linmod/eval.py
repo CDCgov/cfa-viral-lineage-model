7c32241c18176c7db2c01fb8aa97f198d3218345
Thanasi Bakis <abakis@uci.edu>
2024-07-12 15:25:18 -0700

    CRPS implementation

--- linmod/eval.py
+++ linmod/eval.py
@@ -9,8 +9,5 @@ def crps(samples: ArrayLike, truth: float):
 
     return (
         np.abs(samples - truth).mean()
-        - 0.5 * np.abs(samples[::2] - samples[1::2]).mean()
-        # TODO: 10.1007/s11749-008-0114-x uses overlapping pairs of samples
-        # but I am not sure if this is safe
-        # - 0.5 * np.abs(samples[:-1] - samples[1:]).mean()
+        - 0.5 * np.abs(samples[:-1] - samples[1:]).mean()
     )
