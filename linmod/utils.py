7eb4e49a286643490300b701d28a736c55bce4ca
Thanasi Bakis <abakis@uci.edu>
2024-07-31 15:37:57 -0700

    Better way to compute paired differences

--- linmod/utils.py
+++ linmod/utils.py
@@ -13,6 +13,14 @@ def expand_grid(**columns):
     return df.sort(columns.keys())
 
 
+def pl_list_cycle(pl_expr, n: int):
+    assert n > 0
+
+    return pl_expr.list.tail(n).list.concat(
+        pl_expr.list.head(pl_expr.list.len() - n)
+    )
+
+
 def pl_norm(pl_expr, L: int):
     return pl_expr.abs().pow(L).sum().pow(1 / L)
 
