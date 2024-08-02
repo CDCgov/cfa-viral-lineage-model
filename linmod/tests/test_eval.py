84bfc35689795ebe09a3abd8131f206a63b564e7
Thanasi Bakis <abakis@uci.edu>
2024-08-02 10:40:31 -0700

    Hand-derived unit tests

--- linmod/tests/test_eval.py
+++ linmod/tests/test_eval.py
@@ -1,5 +1,7 @@
 import numpy as np
+import polars as pl
 from numpy.random import default_rng
+from polars.testing import assert_frame_equal
 
 from linmod import eval
 from linmod.utils import expand_grid
@@ -20,6 +22,8 @@ def _generate_fake_samples_and_data(
     - Denote true proportion as $\pi_{tgl} = \frac{Y_{tgl}}{Y_{tg \cdot}}$
     - $phi_{tgl} \sim N(\pi_{tgl}, \sigma^2)$
     """
+
+    rng = default_rng(rng)
 
     # Generate fake population counts
     data = expand_grid(
@@ -56,9 +60,9 @@ def test_proportions_mean_L1_norm(
 
     The setup is as follows:
     - Generate fake data $Y_{tgl} \sim Uniform{0, ..., 99}$
-    - Denote true proportion as $\pi_{tgl} = \frac{Y_{tgl}}{Y_{tg \cdot}}$
-    - Generate fake "forecasts" $phi_{tgl} \sim N(\pi_{tgl}, \sigma^2)$
-    - Check $\sum_{t, g} E[ || phi_{tg} - \pi_{tg} ||_1 ]$
+    - Denote true proportion as $\phi_{tgl} = \frac{Y_{tgl}}{Y_{tg \cdot}}$
+    - Generate fake "forecasts" $f_{tgl} \sim N(\phi_{tgl}, \sigma^2)$
+    - Check $\sum_{t, g} E[ || f_{tg} - \phi_{tg} ||_1 ]$
       as reported by `eval.proportions_energy_score`
 
     Note that our "forecasts" are not actually proportions, but independent normal
@@ -94,6 +98,77 @@ def test_proportions_mean_L1_norm(
     )
 
 
+def test_proportions_mean_L1_norm2():
+    samples, data = _generate_fake_samples_and_data(
+        None,
+        num_days=2,
+        num_divisions=2,
+        num_lineages=2,
+        num_samples=2,
+        sample_variance=0,
+    )
+
+    # Put in fixed values for counts and phi
+    data = data.sort("fd_offset", "division", "lineage").with_columns(
+        count=pl.Series([1, 2, 3, 4, 5, 6, 7, 8]),
+    )
+
+    samples = samples.sort(
+        "fd_offset", "division", "sample_index", "lineage"
+    ).with_columns(
+        phi=pl.Series(
+            [
+                0.2,
+                0.8,
+                0.3,
+                0.7,
+                0.4,
+                0.6,
+                0.5,
+                0.5,
+                0.2,
+                0.8,
+                0.3,
+                0.7,
+                0.4,
+                0.6,
+                0.5,
+                0.5,
+            ]
+        ),
+    )
+
+    correct_output = pl.DataFrame(
+        {
+            "fd_offset": [0, 0, 1, 1],
+            "division": [0, 1, 0, 1],
+            "mean_norm": [
+                np.abs(np.array([0.2, 0.3]) - 1 / (1 + 2)).mean()
+                + np.abs(np.array([0.8, 0.7]) - 2 / (1 + 2)).mean(),
+                np.abs(np.array([0.4, 0.5]) - 3 / (3 + 4)).mean()
+                + np.abs(np.array([0.6, 0.5]) - 4 / (3 + 4)).mean(),
+                np.abs(np.array([0.2, 0.3]) - 5 / (5 + 6)).mean()
+                + np.abs(np.array([0.8, 0.7]) - 6 / (5 + 6)).mean(),
+                np.abs(np.array([0.4, 0.5]) - 7 / (7 + 8)).mean()
+                + np.abs(np.array([0.6, 0.5]) - 8 / (7 + 8)).mean(),
+            ],
+        }
+    )
+
+    result = eval.proportions_mean_norm_per_division_day(
+        samples, data, L=1
+    ).collect()
+
+    assert_frame_equal(
+        result,
+        correct_output,
+        check_row_order=False,
+        check_column_order=True,
+        rtol=0,
+        atol=0,
+    )
+
+
 def test_proportions_L1_energy_score(
     num_samples=100000, sample_variance=1.4, atol=0.05
 ):
@@ -102,9 +177,9 @@ def test_proportions_L1_energy_score(
 
     The setup is as follows:
     - Generate fake data $Y_{tgl} \sim Uniform{0, ..., 99}$
-    - Denote true proportion as $\pi_{tgl} = \frac{Y_{tgl}}{Y_{tg \cdot}}$
-    - Generate fake "forecasts" $phi_{tgl} \sim N(\pi_{tgl}, \sigma^2)$
-    - Check $\sum_{t, g} E[ || phi_{tg} - \pi_{tg} ||_2 ] - \frac{1}{2} E[ || phi_{tg} - \phi_{tg}' ||_2 ]$
+    - Denote true proportion as $\phi_{tgl} = \frac{Y_{tgl}}{Y_{tg \cdot}}$
+    - Generate fake "forecasts" $f_{tgl} \sim N(\phi_{tgl}, \sigma^2)$
+    - Check $\sum_{t, g} E[ || f_{tg} - \phi_{tg} ||_2 ] - \frac{1}{2} E[ || f_{tg} - \f_{tg}' ||_2 ]$
       as reported by `eval.proportions_energy_score`
 
     Note that our "forecasts" are not actually proportions, but independent normal
@@ -155,5 +230,116 @@ def test_proportions_L1_energy_score(
     )
 
 
+def test_proportions_L1_energy_score2():
+    samples, data = _generate_fake_samples_and_data(
+        None,
+        num_days=2,
+        num_divisions=2,
+        num_lineages=2,
+        num_samples=3,
+        sample_variance=0,
+    )
+
+    # Put in fixed values for counts and phi
+    data = data.sort("fd_offset", "division", "lineage").with_columns(
+        count=pl.Series([1, 2, 3, 4, 5, 6, 7, 8]),
+    )
+
+    samples = samples.sort(
+        "fd_offset", "division", "sample_index", "lineage"
+    ).with_columns(
+        phi=pl.Series(
+            [
+                0.2,
+                0.8,
+                0.3,
+                0.7,
+                0.1,
+                0.9,
+                0.4,
+                0.6,
+                0.5,
+                0.5,
+                0.1,
+                0.9,
+                0.2,
+                0.8,
+                0.3,
+                0.7,
+                0.1,
+                0.9,
+                0.4,
+                0.6,
+                0.5,
+                0.5,
+                0.1,
+                0.9,
+            ]
+        ),
+    )
+
+    correct_output = pl.DataFrame(
+        {
+            "fd_offset": [0, 0, 1, 1],
+            "division": [0, 1, 0, 1],
+            "energy_score": [
+                np.abs(np.array([0.2, 0.3, 0.1]) - 1 / (1 + 2)).mean()
+                + np.abs(np.array([0.8, 0.7, 0.9]) - 2 / (1 + 2)).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.2, 0.3, 0.1]) - np.array([0.1, 0.2, 0.3])
+                ).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.8, 0.7, 0.9]) - np.array([0.9, 0.8, 0.7])
+                ).mean(),
+                np.abs(np.array([0.4, 0.5, 0.1]) - 3 / (3 + 4)).mean()
+                + np.abs(np.array([0.6, 0.5, 0.9]) - 4 / (3 + 4)).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.4, 0.5, 0.1]) - np.array([0.1, 0.4, 0.5])
+                ).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.6, 0.5, 0.9]) - np.array([0.9, 0.6, 0.5])
+                ).mean(),
+                np.abs(np.array([0.2, 0.3, 0.1]) - 5 / (5 + 6)).mean()
+                + np.abs(np.array([0.8, 0.7, 0.9]) - 6 / (5 + 6)).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.2, 0.3, 0.1]) - np.array([0.1, 0.2, 0.3])
+                ).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.8, 0.7, 0.9]) - np.array([0.9, 0.8, 0.7])
+                ).mean(),
+                np.abs(np.array([0.4, 0.5, 0.1]) - 7 / (7 + 8)).mean()
+                + np.abs(np.array([0.6, 0.5, 0.9]) - 8 / (7 + 8)).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.4, 0.5, 0.1]) - np.array([0.1, 0.4, 0.5])
+                ).mean()
+                - 0.5
+                * np.abs(
+                    np.array([0.6, 0.5, 0.9]) - np.array([0.9, 0.6, 0.5])
+                ).mean(),
+            ],
+        }
+    )
+
+    result = eval.proportions_energy_score_per_division_day(
+        samples, data, L=1
+    ).collect()
+
+    assert_frame_equal(
+        result,
+        correct_output,
+        check_row_order=False,
+        check_column_order=True,
+    )
+
+
 test_proportions_mean_L1_norm()
 test_proportions_L1_energy_score()
+test_proportions_mean_L1_norm2()
+test_proportions_L1_energy_score2()
