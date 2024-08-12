71849bf435b09ab25a690d5ab06afa7136bb6889
Thanasi Bakis <abakis@uci.edu>
2024-08-12 09:14:49 -0700

    Some docstrings on util functions

--- linmod/utils.py
+++ linmod/utils.py
@@ -3,8 +3,13 @@ from functools import reduce
 import polars as pl
 
 
-# Like the R version
 def expand_grid(**columns):
+    """
+    Create a DataFrame from all combinations of given columns.
+
+    Operates like the R function `tidyr::expand_grid`.
+    """
+
     column_dfs = map(
         lambda c: pl.DataFrame(c[1], schema=[c[0]]), columns.items()
     )
@@ -14,6 +19,11 @@ def expand_grid(**columns):
 
 
 def pl_list_cycle(pl_expr, n: int):
+    """
+    Returns the column computed by `pl_expr`, but with the last `n` elements
+    moved to the front.
+    """
+
     assert n > 0
 
     return pl_expr.list.tail(n).list.concat(
@@ -21,9 +31,15 @@ def pl_list_cycle(pl_expr, n: int):
     )
 
 
-def pl_norm(pl_expr, L: int):
-    return pl_expr.abs().pow(L).sum().pow(1 / L)
+def pl_norm(pl_expr, p: int):
+    r"""
+    Computes the L_p norm $||\cdot||_p$ of the column `pl_expr`.
+    """
+    return pl_expr.abs().pow(p).sum().pow(1 / p)
 
 
 def pl_softmax(pl_expr):
+    """
+    Computes the softmax of the column `pl_expr`.
+    """
     return pl_expr.exp() / pl_expr.exp().sum()
