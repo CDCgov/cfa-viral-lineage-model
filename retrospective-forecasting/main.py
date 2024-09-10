39c52c50ff6d9aa137c3121d3f32efce4340667f
Thanasi Bakis <abakis@uci.edu>
2024-09-10 10:51:27 -0700

    First thoughts on domination time eval

--- retrospective-forecasting/main.py
+++ retrospective-forecasting/main.py
@@ -166,44 +166,70 @@ with open(plot_script_file, "w") as plot_script:
     eval_dir = ValidPath(config["evaluation"]["save_dir"])
 
     scores = []
-    for metric_name in config["evaluation"]["metrics"]:
-        metric_function = getattr(linmod.eval, metric_name)
 
-        for forecast_path in forecast_dir.glob("forecasts_*.parquet"):
-            model_name = forecast_path.stem.split("_")[1]
-            print_message(
-                f"Evaluating {model_name} model using {metric_name}...", end=""
+    for forecast_path in forecast_dir.glob("forecasts_*.parquet"):
+        model_name = forecast_path.stem.split("_")[1]
+        forecast = pl.scan_parquet(forecast_path)
+
+        for evaluator_config in config["evaluation"]["metrics"]:
+            if type(evaluator_config) is dict:
+                assert (
+                    len(evaluator_config) == 1
+                ), "Evaluator config is formatted incorrectly."
+
+                evaluator_config = list(evaluator_config.items())
+                evaluator_name = evaluator_config[0][0]
+                evaluator_args = {
+                    k: v for d in evaluator_config[0][1] for k, v in d.items()
+                }
+
+            else:
+                evaluator_name = evaluator_config
+                evaluator_args = {}
+
+            evaluator = getattr(linmod.eval, evaluator_name)(
+                samples=forecast,
+                data=eval_data.lazy(),
+                **evaluator_args,
             )
 
-            forecast = pl.read_parquet(forecast_path)
+            for metric_name, metric_function in vars(type(evaluator)).items():
+                if metric_name.startswith("_"):
+                    continue
 
-            forecast = forecast.lazy()
-            scores.append(
-                (
-                    metric_name,
-                    model_name,
-                    metric_function(forecast, eval_data.lazy()),
-                )
-            )
-
-            if (
-                metric_name == config["evaluation"]["metrics"][0]
-            ):  # One plot per model
-                data_path = config["data"]["save_file"]["eval"]
-                png_path = (
-                    eval_dir / "visualizations" / f"eval_{model_name}.png"
-                )
-                plot_script.write(
+                print_message(
                     (
-                        "python3 -m linmod.visualize "
-                        f"-f {forecast_path} "
-                        f"-d {data_path} "
-                        f"-p {png_path} "
-                        "-t eval\n"
+                        f"Evaluating {model_name} model using "
+                        f"{evaluator_name}.{metric_name}..."
+                    ),
+                    end="",
+                )
+
+                scores.append(
+                    (
+                        f"{evaluator_name}.{metric_name}",
+                        model_name,
+                        metric_function(evaluator),
                     )
                 )
 
-            print_message(" done.")
+                print_message(" done.")
+
+        data_path = config["data"]["save_file"]["eval"]
+        png_path = (
+            eval_dir / "visualizations" / f"eval_{model_name}.png"
+        )
+        plot_script.write(
+            (
+                "python3 -m linmod.visualize "
+                f"-f {forecast_path} "
+                f"-d {data_path} "
+                f"-p {png_path} "
+                "-t eval\n"
+            )
+        )
+        
+
 
 print_message("Success!")
 
